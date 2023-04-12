3fbfe0177bbdcc865525b67860178a1f1ec6283f
justNuka <75932540+justNuka@users.noreply.github.com>
2023-04-12 15:34:19 +0200

    push

--- server.js
+++ server.js
@@ -1,24 +1,30 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const fs = require('fs');
-
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
+
 app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/leaderboard', (req, res) => {
+  const leaderboardData = JSON.parse(fs.readFileSync('leaderboard.json'));
+  res.send(leaderboardData);
+});
+
+app.get('/hello', (req, res) => {
+  res.send('Hello World!');
+});
 
 app.post('/leaderboard', (req, res) => {
-  const data = req.body;
-
-  // Récupérer les données existantes du fichier JSON
-  const leaderboard = JSON.parse(fs.readFileSync('leaderboard.json'));
-
-  // Ajouter les nouvelles données du joueur
-  leaderboard.push(data);
-
-  // Enregistrer les nouvelles données dans le fichier JSON
-  fs.writeFileSync('leaderboard.json', JSON.stringify(leaderboard));
-
-  res.sendStatus(200);
+  const newScore = req.body;
+  const leaderboardData = JSON.parse(fs.readFileSync('leaderboard.json'));
+  leaderboardData.push(newScore);
+  fs.writeFileSync('leaderboard.json', JSON.stringify(leaderboardData));
+  res.send('Score ajouté avec succès');
 });
+
+
 
 app.listen(3000, () => {
   console.log('Serveur démarré sur le port 3000');
